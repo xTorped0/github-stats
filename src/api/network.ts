be365edc07c7e4413f7cef878e0993de4b0ca441
Torped0 <dabizha.vanya@gmail.com>
2024-04-26 18:19:41 +0300

    github token was removed

--- src/api/network.ts
+++ src/api/network.ts
@@ -1,13 +1,13 @@
 import axios, { type CreateAxiosDefaults } from 'axios';
 
 const baseURL = import.meta.env.VITE_API_URL;
-const token = import.meta.env.VITE_GITHUB_TOKEN;
+// const token = import.meta.env.VITE_GITHUB_TOKEN;
 
 const options: CreateAxiosDefaults = {
 	baseURL,
 	headers: {
 		'Content-Type': 'application/json',
-		Authorization: `token ${token}`,
+		// Authorization: `token ${token}`,
 	},
 	withCredentials: false // Server cookies are not used
 };
